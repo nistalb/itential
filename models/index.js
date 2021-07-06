91d3cf574ee01aff3c8cfbabd355d5ffe175b4e5
nistalb <brent.daddy@gmail.com>
2021-07-05 21:33:03 -0700

    completes models index.js file

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// connections string
+const dburl = "mongodb://localhost:27017/itentialdb"
+
+// connect
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+.then(function(){
+    console.log("MongoDB connected");
+})
+.catch(function(error){
+    console.log("MongoDB error");
+    console.log(error);
+});
+
+mongoose.connection.on("disconnected", function(){
+    console.log("MongoDB disconnected");
+});
+
+module.exports= {
+    Soda: require("./Soda"),
+    Machine: require("./Machine"),
+    User: require("./User")
+};
