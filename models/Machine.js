84e04ff27b445ca388bc6a718e5f333ce65b0d3b
nistalb <brent.daddy@gmail.com>
2021-07-06 11:25:25 -0700

    adds seed data to populate database

--- models/Machine.js
+++ models/Machine.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 // create schema for Machine documents
 const machineSchema = new mongoose.Schema({
-    location: String,
     soda: [ {
         id: {type: mongoose.Schema.Types.ObjectId, ref: "Soda"},
         qty: { type: Number, required: true},
